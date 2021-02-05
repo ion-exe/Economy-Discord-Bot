5be40e76810dfcfaea051858c6649aba12cb03c7
Anonymous <70274221+ion-exe@users.noreply.github.com>
2021-02-05 12:09:38 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const FS = require('fs');
+const db = require('quick.db');
+client.commands = new Discord.Collection();
+FS.readdir("./commands/", (e, files) => {
+
+  if(e) console.log(JSON.stringify(e));
+  let file = files.filter(f => f.split(".").pop() === "js");
+  file.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+ });
+})
+
+client.on('ready', ()=>console.log('ready'));
+client.on('message', function(message) {
+  if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    let prefix = '!'
+    let messageArray = message.content.split(" ");
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+    let cmdfile;
+
+    if (client.commands.has(cmd)) {
+      cmdfile = client.commands.get(cmd);
+      try {
+        cmdfile.run(client, message, args)
+      } catch (e) {
+        console.log(JSON.stringify(e))
+      }
+    }
+})
+
+  
+
+client.login(process.env.DISCORD_TOKEN)
